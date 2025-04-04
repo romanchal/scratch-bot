7987bfb869076275fa47de44246b41439f5f8044
romanchal <romanchalbopche@gmail.com>
2025-04-05 00:36:48 +0530

    r
    r

--- functions/src/handlers/addQueue.ts
+++
@@ -1,7 +0,0 @@
-import { addQueue } from '../modules/firestore';
-import { SlackEventInterface } from '../utils/interfaces';
-
-export async function addQueueHandler(event: SlackEventInterface): Promise<void> {
-  await addQueue(event);
-  return;
-}
